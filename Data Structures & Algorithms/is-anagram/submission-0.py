96efb9e36a62a864de0f0f924639c4d81b3505b5
Jiří Pokorný <61350724+Jpoker01@users.noreply.github.com>
2026-07-14 01:32:12 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,13 @@
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dic = Counter(s)
+        t_dic = Counter(t)
+        if s_dic == t_dic:
+            return True
+        else:
+            return False
+                    
+
+        
